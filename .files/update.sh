81acb0438b93d70ffdccf37246a7db1dd4bb55b2
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-15 08:36:37 +0530

    Update update.sh

--- .files/update.sh
+++ .files/update.sh
@@ -18,6 +18,15 @@ echo -ne "$B[$N››››.........$B] $BR[Checking]\r"
 sleep 0.1
 echo -ne "$B[$N›››››........$B] $BR[Checking]\r"
 sleep 0.1
+connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+if [[ "$connection" != 0 ]]
+then echo -ne "\033[0m"
+sleep 1
+echo -ne "\033[0m\033[1;91m[\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!\033[0m\r"
+echo -ne "\033[0m"
+sleep 2
+exit
+fi
 echo -ne "$B[$N››››››.......$B] $BG[Founded!]\r"
 sleep 0.6
 echo -ne "$B[$N›››››››......$B] $BG[Updating]\r"
