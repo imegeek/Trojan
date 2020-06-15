4fe1a58936115569781a745532fc4f7a3d06273a
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-15 19:34:32 +0530

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -4,13 +4,13 @@ clear
 command -v ruby > /dev/null 2>&1 || { echo >&2 "I require ruby but it's not installed, Now Installing."; apt install ruby -y; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then clear
-echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+echo -e "\033[1;91m[\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
    exit
 fi; }
 command -v lolcat > /dev/null 2>&1 || { echo >&2 "I require lolcat but it's not installed, Now Installing."; gem install lolcat; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then clear
-echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+echo -e "\033[1;91m[\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
    exit
 fi; }
 
