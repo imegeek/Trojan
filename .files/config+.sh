2e8c933f43d0c047dfe073b1b30c394dbcd2b307
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-16 07:01:39 +0530

    Update config+.sh

--- .files/config+.sh
+++ .files/config+.sh
@@ -24,6 +24,7 @@ cd /sdcard
 mkdir Download
 fi
 if [ ! -f '/storage/emulated/0/Download/saver_1.0.apk' ];then
+tput civis
 echo -ne "$R|█                               |  (0%)\r"
 sleep 0.5
 echo -ne "$R|█                               |  (10%)\r"
@@ -97,4 +98,5 @@ sleep 0.5
 echo -ne "$G[⬇]Antivirus Saved To Dir : emulated/0/Download\r"
 sleep 2
 echo -ne '\n'
+tput cnorm
 fi
