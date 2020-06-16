7be5c12c0f09188b17c3b5d851b199d285d736a2
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-16 07:13:34 +0530

    Update update.sh

--- .files/update.sh
+++ .files/update.sh
@@ -6,17 +6,17 @@ B='\033[1;94m' #Blue
 BR='\033[0m\033[1;101m' #Background #Red
 BG='\033[0m\033[1;30;102m' #Background #Green
 tput civis
-echo -ne "$B[$N.............$B] $BR[Checking]\r"
+echo -ne "$N$B[$N.............$B] $BR[Checking]\r"
 sleep 0.1
-echo -ne "$B[$N›............$B] $BR[Checking]\r"
+echo -ne "$N$B[$N›............$B] $BR[Checking]\r"
 sleep 0.1
-echo -ne "$B[$N››...........$B] $BR[Checking]\r"
+echo -ne "$N$B[$N››...........$B] $BR[Checking]\r"
 sleep 0.1
-echo -ne "$B[$N›››..........$B] $BR[Checking]\r"
+echo -ne "$N$B[$N›››..........$B] $BR[Checking]\r"
 sleep 0.1
-echo -ne "$B[$N››››.........$B] $BR[Checking]\r"
+echo -ne "$N$B[$N››››.........$B] $BR[Checking]\r"
 sleep 0.1
-echo -ne "$B[$N›››››........$B] $BR[Checking]\r"
+echo -ne "$N$B[$N›››››........$B] $BR[Checking]\r"
 sleep 0.1
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
@@ -27,19 +27,19 @@ echo -ne "\033[0m"
 sleep 2
 exit
 fi
-echo -ne "$B[$N››››››.......$B] $BG[Founded!]\r"
+echo -ne "$N$B[$N››››››.......$B] $BG[Founded!]\r"
 sleep 0.6
 cd .. && cd .. && rm -rf Trojan
-echo -ne "$B[$N›››››››......$B] $BG[Updating]\r"
+echo -ne "$N$B[$N›››››››......$B] $BG[Updating]\r"
 sleep 0.1
-echo -ne "$B[$N›››››››››....$B] $BG[Updating]\r"
+echo -ne "$N$B[$N›››››››››....$B] $BG[Updating]\r"
 sleep 0.1
-echo -ne "$B[$N››››››››››...$B] $BG[Updating]\r"
+echo -ne "$N$B[$N››››››››››...$B] $BG[Updating]\r"
 sleep 0.1
 git clone https://github.com/abhackerofficial/Trojan > /dev/null 2>&1
-echo -ne "$B[$N›››››››››››..$B] $BG[Updating]\r"
+echo -ne "$N$B[$N›››››››››››..$B] $BG[Updating]\r"
 sleep 0.1
-echo -ne "$B[$N››››››››››››.$B] $BG[Updating]\r"
+echo -ne "$N$B[$N››››››››››››.$B] $BG[Updating]\r"
 sleep 0.1
 echo -ne "$N$B[$N›››››››››››››$B] $BG[Update Compelete]\r"
 sleep 0.6
