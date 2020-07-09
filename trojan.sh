0736930e6cf4057b18cd3bb8c8248b5490b39014
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-07-09 09:45:54 +0530

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -1,6 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 clear
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed, Now Installing."; apt install php -y; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+if [[ "$connection" != 0 ]]
+then clear
+echo -e "\033[1;91m[\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+   exit
+fi; }
 command -v ruby > /dev/null 2>&1 || { echo >&2 "I require ruby but it's not installed, Now Installing."; apt install ruby -y; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then clear
