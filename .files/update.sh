93e933fa7e5f471b9a0ba9fd10679d087f11e5f0
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-08-18 05:41:00 +0530

    Update update.sh

--- .files/update.sh
+++ .files/update.sh
@@ -1,23 +1,66 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 #[colors] #Blod
-N='\033[0m\033[1m' #NoColor
-B='\033[1;94m' #Blue
-BR='\033[0m\033[1;101m' #Background #Red
+N='\033[0m\033[1m'         #NoColor
+B='\033[1;94m'             #Blue
+BL='\033[1;30m'            #Black
+BR='\033[0m\033[1;101m'    #Background #Red
 BG='\033[0m\033[1;30;102m' #Background #Green
-tput civis
-echo -ne "$N$B[$N.............$B] $BR[Checking]\r"
-sleep 0.1
-echo -ne "$N$B[$N›............$B] $BR[Checking]\r"
-sleep 0.1
-echo -ne "$N$B[$N››...........$B] $BR[Checking]\r"
-sleep 0.1
-echo -ne "$N$B[$N›››..........$B] $BR[Checking]\r"
-sleep 0.1
-echo -ne "$N$B[$N››››.........$B] $BR[Checking]\r"
-sleep 0.1
-echo -ne "$N$B[$N›››››........$B] $BR[Checking]\r"
-sleep 0.1
+
+
+HIDCRSR(){ echo -en "\033[?25l";}
+NORM(){ echo -en "\033[?12l\033[?25h";}
+
+HIDCRSR
+trap '' 2
+echo -ne "$N[$BL.............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$BL.............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$BL.............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█$BL............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█$BL............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█$BL............$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N██$BL...........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N██$BL...........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N██$BL...........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N███$BL..........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N███$BL..........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N███$BL..........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N████$BL.........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N████$BL.........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N████$BL.........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking]\r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking]\r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking]\r"
+sleep 0
+echo -ne "$N[$N█████$BL........$N] $BR[Checking] \r"
+sleep 0
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then echo -ne "\033[0m"
@@ -27,22 +70,203 @@ echo -ne "\033[0m"
 sleep 2
 exit
 fi
-echo -ne "$N$B[$N››››››.......$B] $BG[Founded!]\r"
-sleep 0.6
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
+echo -ne "$N[$N██████$BL.......$N] $BG[Founded!] \r"
+sleep 0
 cd .. && cd .. && rm -rf Trojan
-echo -ne "$N$B[$N›››››››......$B] $BG[Updating]\r"
-sleep 0.1
-echo -ne "$N$B[$N›››››››››....$B] $BG[Updating]\r"
-sleep 0.1
-echo -ne "$N$B[$N››››››››››...$B] $BG[Updating]\r"
-sleep 0.1
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████$BL......$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████$BL.....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N█████████$BL....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N█████████$BL....$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N█████████$BL....$N] $BG[Updating] \r"
+sleep 0
 git clone https://github.com/abhackerofficial/Trojan > /dev/null 2>&1
-echo -ne "$N$B[$N›››››››››››..$B] $BG[Updating]\r"
-sleep 0.1
-echo -ne "$N$B[$N››››››››››››.$B] $BG[Updating]\r"
-sleep 0.1
-echo -ne "$N$B[$N›››››››››››››$B] $BG[Update Compelete]\r"
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N██████████$BL...$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N███████████$BL..$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N████████████$BL.$N] $BG[Updating] \r"
+sleep 0
+echo -ne "$N[$N█████████████$BL$N] $BG[Update Compelete] \r"
+sleep 0
+echo -ne "$N[$N█████████████$BL$N] $BG[Update Compelete] \r"
+sleep 0
+echo -ne "$N[$N█████████████$BL$N] $BG[Update Compelete] \r"
 sleep 0.6
 echo -ne '\033[0m\n'
-tput cnorm
+trap 5
+NORM
 cd Trojan && bash trojan.sh
